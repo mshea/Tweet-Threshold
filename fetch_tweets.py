4da96eb52503ec99af4b3ea41130b2886ede33b1
Mike Shea <mike@mikeshea.net>
2013-04-07 22:31:31 -0400

    OO Redesign
    Completely refactored the code to OO.

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,52 +1,31 @@
 import tweet_threshold
 
-PARAMS = {
-	'DB_FILE': '/path/to/your/db/tweet_threshold.sqlite',
-	'HTML_TEMPLATE': '/path/to/your/www/html_template.txt',
-	'HTML_OUTPUT': '/path/to/your/www/index.html'
-	'SCORE_THRESHOLD': 50,
-	'BLACKLIST': [
-			'Congress', 
-			'Representative',
-			'DHS', 
-			'Fox', 
-			'CISPA',
-			'Republicans'
-		],
-	'WHITELIST': [
-			'twitter_account_you_like',
-			'twitter_account_you_like',
-			'twitter_account_you_like'
-		]
-}
-
 TWITTER_ACCOUNT_DATA = [
 	{
-	'consumer_key': 'keyhash',
-	'consumer_secret': 'keyhash',
-	'access_token_key': 'keyhash',
-	'access_token_secret': 'keyhash',
+	'consumer_key': 'yourkeyhere',
+	'consumer_secret': 'yourkeyhere',
+	'access_token_key': 'yourkeyhere',
+	'access_token_secret': 'yourkeyhere',
 	}, {
-	'consumer_key': 'keyhash',
-	'consumer_secret': 'keyhash',
-	'access_token_key': 'keyhash',
-	'access_token_secret': 'keyhash',
+	'consumer_key': 'yourkeyhere',
+	'consumer_secret': 'yourkeyhere',
+	'access_token_key': 'yourkeyhere',
+	'access_token_secret': 'yourkeyhere',
 	}
 ]
+	
+PARAMS = {
+	'db': '/your/path/here/tweet_threshold.sqlite',
+	'html_output': '/your/path/here/news.html',
+	'html_template': '/your/path/here/html_template.txt',
+	'blacklist': (
+		'asdf',
+		'asdfasdf'
+		),
+	'whitelist': (
+		'mshea',
+		'slyflourish'
+		)
+	}
 
-def main(TWITTER_ACCOUNT_DATA, PARAMS):
-	for account in TWITTER_ACCOUNT_DATA:
-		tweets = tweet_threshold.get_tweets(account)
-		tweet_threshold.export_to_sqlite(tweets, PARAMS['DB_FILE'])
-	output_tweets = tweet_threshold.load_tweets_from_sqlite(PARAMS['DB_FILE'])
-	tweet_threshold.build_html_page(
-			output_tweets, 
-			PARAMS['HTML_TEMPLATE'],
-			PARAMS['HTML_OUTPUT'], 
-			PARAMS['SCORE_THRESHOLD'], 
-			PARAMS['WHITELIST'],
-			PARAMS['BLACKLIST'])
-	tweet_threshold.purge_database(PARAMS['DB_FILE'])
-
-if __name__ == "__main__":
-    main(TWITTER_ACCOUNT_DATA, PARAMS)
+tweet_threshold.main(TWITTER_ACCOUNT_DATA, PARAMS)
--- tweet_threshold.py
+++ tweet_threshold.py
@@ -7,144 +7,188 @@ import datetime
 import math
 import jinja2
 
-def get_tweets(account_data):
-	auth = tweepy.auth.OAuthHandler(account_data['consumer_key'],
-			account_data['consumer_secret'])
-	auth.set_access_token(account_data['access_token_key'],
-			account_data['access_token_secret'])
-	api = tweepy.API(auth)
-	tweet_data = []
-	for tweet in api.home_timeline(count=100, include_rts=0):
-		id = tweet.id_str
+class Tweets (object):
+	def __init__(self, account_data, params):
+		auth = tweepy.auth.OAuthHandler(account_data['consumer_key'],
+				account_data['consumer_secret'])
+		auth.set_access_token(account_data['access_token_key'],
+				account_data['access_token_secret'])
+		api = tweepy.API(auth)
+		self.db = params['db']
+		self.tweets = []
+		for tweet in api.home_timeline(count=100, include_rts=0):
+			try:
+				url = tweet.entities['urls'][0]['expanded_url']
+			except IndexError:
+				url = False			
+			if (url is not False):
+				self.tweets.append((
+						tweet.id_str,
+						self.extract_urls(tweet.text),
+						url,
+						str(tweet.created_at).replace(' ','T'),
+						tweet.retweet_count,
+						tweet.user.screen_name,
+						tweet.user.followers_count
+						))
+
+	def save(self):
+		tdb = TweetDatabase(self.db)
+		tdb.save(self.tweets)
+		tdb.purge()
+
+	def extract_urls(self, text):
 		text = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'\
 				'[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','', 
-						tweet.text).strip()
-		text = re.sub('\:$','',text)
+				text).strip()
+		text = re.sub('\:$','',text)	
+		return text
+		
+class TweetDatabase (object):
+	def __init__(self, db):
+		self.conn = sqlite3.connect(db)
+		self.c = self.conn.cursor()
+		
+	def create(self):
 		try:
-			url = tweet.entities['urls'][0]['expanded_url']
-		except IndexError:
-			url = False
-		created_at = str(tweet.created_at).replace(' ','T')
-		retweet_count = tweet.retweet_count
-		screen_name = tweet.user.screen_name
-		followers_count = tweet.user.followers_count
-		if (url is not False):
-			tweet_data.append((id, text, url, created_at, retweet_count, 
-					screen_name, followers_count))
-	return tweet_data
-
-def export_to_sqlite(data, db_file):
-	conn = sqlite3.connect(db_file)
-	c = conn.cursor()
-	try: # Data Model Below
-		c.execute('''CREATE TABLE tweets 
-				( id int not null unique,
-				text text, 
-				url text, 
-				created_at text, 
-				retweet_count int,
-				screen_name text,
-				followers_count int);''')
-	except:
-		error = "table already created..."
-	c.executemany('''INSERT OR REPLACE INTO tweets VALUES (?,?,?,?,?,?,?)''', 
-					data)
-	conn.commit()
-	return True
-
-def load_tweets_from_sqlite(db_file):
-	conn = sqlite3.connect(db_file)
-	c = conn.cursor()
-	full_results = []
-	tweetquery = conn.cursor()
-	tweetquery.execute('''
-			select * from tweets 
-			where datetime(created_at) > date('now','-7 day')
-			;''')
-	for result in tweetquery:
-		id, text, url, created_at, retweet_count, screen_name, \
-			followers_count = result
-		full_results.append(result)
-	return full_results
-	
-def get_score(retweet_count, followers_count): # The secret sause algorithm
-	retweet_count -= 1 # lower score for low-follower users
-	if retweet_count > 2:
-		retweet_score = pow(retweet_count, 1.5) # boost retweets
-		raw_score = (retweet_score / followers_count)*100000 # Build score
-		score = round(math.log(raw_score, 1.09)) # Smooth out score
-	else:
-		score = 0
-	return score
-
-def build_html_page(data, html_template, html_output_file, threshold, 
-		whitelist, blacklist):
-	scored_tweets = []
-	yesterdays_items = []
-	last_weeks_items = []
-	last_week_item_counter = 40
-	yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)) \
-			.replace(hour=0, minute=0, second=0, microsecond=0)
-	for item in data:
-		id, text, url, created_at, retweet_count, screen_name, \
-			followers_count = item
-		score = get_score(retweet_count, followers_count)
-		scored_tweets.append((text, url, score, screen_name, created_at, 
-				retweet_count, followers_count))
-	sorted_tweets = sorted(scored_tweets, key=lambda 
-			tup: tup[2], reverse=True)
-	for item in sorted_tweets:
-		text, url, score, screen_name, created_at, retweet_count, \
-				followers_count = item
-		created_at_object = datetime.datetime.strptime(created_at, 
-				'%Y-%m-%dT%H:%M:%S').replace(hour=0, minute=0, 
-				second=0, microsecond=0)
-		formatted_time = time.strftime('%I:%M %p', 
-				time.strptime(created_at, '%Y-%m-%dT%H:%M:%S'))
-		if ((score >= threshold or check_whitelist(screen_name, whitelist))
-				and yesterday == created_at_object 
-				and check_blacklist(text, blacklist)):
-			yesterdays_items.append({'text': text.strip(), 'url': url, 
-					'score': str(int(score)), 'screen_name': screen_name, 
-					'created_at_time': formatted_time, 
-					'retweet_count': retweet_count, 
-					'followers_count': followers_count})
-		if ((score >= threshold or check_whitelist(screen_name, whitelist))
-				and created_at_object < yesterday 
-				and last_week_item_counter > 0
-				and check_blacklist(text, blacklist)):
-			last_weeks_items.append({'text': text, 'url': url, 
-					'score': str(int(score)), 'screen_name': screen_name, 
-					'created_at_time': formatted_time, 
-					'retweet_count': retweet_count, 
-					'followers_count': followers_count})
-			last_week_item_counter -= 1
-	with open(html_template) as f: 
-		template = jinja2.Template(f.read())
-	html_output = template.render(yesterdays_items = yesterdays_items, 
-			last_weeks_items = last_weeks_items)
-	with open(html_output_file, 'w') as f: 
-		f.write(html_output.encode('utf-8'))
-	return True
-
-def check_blacklist(text, BLACKLIST):
-	for phrase in BLACKLIST:
-		if phrase.strip() in text:
-			return False
-	return True
-	
-def check_whitelist(screen_name, WHITELIST):
-	for whitelist_name in WHITELIST:
-		if screen_name == whitelist_name:
+			self.c.execute('''CREATE TABLE tweets 
+									( id int not null unique,
+									text text, 
+									url text, 
+									created_at text, 
+									retweet_count int,
+									screen_name text,
+									followers_count int);''')
+			self.conn.commit()
 			return True
-	return False
+		except sqlite3.OperationalError:
+			return False
+
+	def load(self):
+		self.results = []
+		self.c.execute('''select * from tweets;''')
+		for result in self.c:
+			id, text, url, created_at, retweet_count, screen_name, \
+				followers_count = result
+			self.results.append(result)
+		return self.results
 	
-def purge_database(db_file):
-	conn = sqlite3.connect(db_file)
-	cleandatabase = conn.cursor()
-	cleandatabase.execute('''
+	def save(self, data):
+		self.create()
+		self.c.executemany('''INSERT OR REPLACE INTO tweets 
+					VALUES (?,?,?,?,?,?,?)''', data)
+		self.conn.commit()
+		return True
+		
+	def purge(self):
+		self.c.execute('''
 			delete from tweets
 			where datetime(created_at) < date('now','-8 day');
 			''')
-	cleandatabase.execute('vacuum;')
-	conn.commit()
+		self.c.execute('vacuum;')
+		self.conn.commit()
+		return True
+	
+class FilteredTweets (object):
+	def __init__(self, params):
+		self.filtered_tweets = []
+		self.blacklist = params['blacklist']
+		self.whitelist = params['whitelist']
+		self.db = TweetDatabase(params['db'])
+		self.yesterday_morning = (
+				datetime.datetime.today() - 
+				datetime.timedelta(days=1)).replace(
+						hour=0, minute=0, second=0, microsecond=0)
+		self.yesterday_evening = (
+				datetime.datetime.today() - 
+				datetime.timedelta(days=1)).replace(
+						hour=23, minute=59, second=59, microsecond=0)
+		self.last_week_date = (
+				datetime.datetime.today() - 
+				datetime.timedelta(days=7)).replace(
+						hour=23, minute=59, second=59, microsecond=0)
+		self.tweets = self.db.load()
+		for tweet in self.tweets:
+			id, text, url, created_at, retweet_count, screen_name, \
+					followers_count = tweet
+			score = self.build_score(retweet_count, followers_count)
+			if (self.check_blacklist(text) and (score > 50 or 
+					self.check_whitelist(screen_name))):
+				self.filtered_tweets.append(
+						{
+						'id': id, 
+						'text': text, 
+						'url': url, 
+						'created_at': created_at,
+						'retweet_count': retweet_count, 
+						'screen_name': screen_name, 
+						'followers_count': followers_count, 
+						'score': score
+						})
+			self.filtered_tweets = sorted(self.filtered_tweets, key=lambda 
+			tup: tup['score'], reverse=True)
+
+	def check_blacklist(self, text):
+		for phrase in self.blacklist:
+			if phrase.strip() in text:
+				return False
+		return True
+	
+	def check_whitelist(self, screen_name):
+		for whitelist_name in self.whitelist:
+			if screen_name == whitelist_name:
+				return True
+		return False
+
+	def build_score(self, retweet_count, followers_count):
+		retweet_count -= 1
+		if retweet_count > 2:
+			retweet_score = pow(retweet_count, 1.5)
+			raw_score = (retweet_score / followers_count)*100000
+			score = round(math.log(raw_score, 1.09))
+		else:
+			score = 0
+		return int(score)
+
+	def load_yesterday(self):
+		self.yesterdays_tweets=[]
+		for tweet in self.filtered_tweets:
+			self.created_at_object = datetime.datetime.strptime(
+					tweet['created_at'], '%Y-%m-%dT%H:%M:%S')
+			if (self.created_at_object > self.yesterday_morning 
+					and self.created_at_object < self.yesterday_evening):
+				self.yesterdays_tweets.append(tweet)
+		return self.yesterdays_tweets
+	
+	def load_last_week(self):
+		self.last_weeks_tweets=[]
+		for tweet in self.filtered_tweets:
+			self.created_at_object = datetime.datetime.strptime(
+					tweet['created_at'], '%Y-%m-%dT%H:%M:%S')
+			if (self.created_at_object < self.yesterday_morning 
+					and self.created_at_object > self.last_week_date):
+				self.last_weeks_tweets.append(tweet)
+		return self.last_weeks_tweets
+	
+class WebPage (object):
+	def __init__(self, params):
+		self.html_output_file = params['html_output']
+		self.html_template = params['html_template']
+
+	def build(self, yesterdays_items, last_weeks_items):
+		with open(self.html_template) as f: 
+			template = jinja2.Template(f.read())
+		self.html_output = template.render(yesterdays_items = yesterdays_items, 
+				last_weeks_items = last_weeks_items)
+		with open(self.html_output_file, 'w') as f: 
+			f.write(self.html_output.encode('utf-8'))
+		return True
+		
+def main(accounts, params):
+	for account in accounts:
+		remote_tweets = Tweets(account, params)
+		remote_tweets.save()
+	tweets = FilteredTweets(params)
+	wp = WebPage(params)
+	wp.build(tweets.load_yesterday(), tweets.load_last_week())
+
